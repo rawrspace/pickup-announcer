23e83888e73361f28bc1f0176de51edc220b3e52
CJ van der Smissen <vandersmissenc@gmail.com>
2020-08-12 09:11:02 -0500

    Add project files.

---
+++ PickupAnnouncer/Hubs/PickupHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace PickupAnnouncer.Hubs
+{
+    public class PickupHub : Hub
+    {
+        public async Task AnnouncePickup(string carId, string coneId)
+        {
+            await Clients.All.SendAsync("PickupAnnouncement", carId, coneId);
+        }
+    }
+}
